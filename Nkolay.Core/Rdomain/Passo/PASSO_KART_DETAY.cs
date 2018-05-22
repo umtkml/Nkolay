a466d402b4d7f62ba6a4202bc9be35bb8f138587
umtkml <34253013+umtkml@users.noreply.github.com>
2018-05-22 09:59:51 +0300

    Nkolay Core

---
+++ Nkolay.Core/Rdomain/Passo/PASSO_KART_DETAY.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nkolay.Web.Infrastructure.Core;
+
+namespace Nkolay.Web.Api.Core.Rdomain.Passo
+{
+    public class PassoKartDetay : REntity
+    {
+        public int Bayi_Id { get; set; }
+        public decimal Bayi_Limit { get; set; }
+        public decimal Bayi_Kredi_Limit { get; set; }
+        public bool Tarih_Durum { get; set; }
+        public DateTime Limit_GecerlilikTarihi { get; set; }
+        public string Kod { get; set; }
+        public int Para_Birim_Id { get; set; }
+        public int Row_Ver { get; set; }
+        public bool Kilit { get; set; }
+        public decimal Limit_Uyari_Ust { get; set; }
+        public decimal Limit_Uyari_Alt { get; set; }
+
+    }
+}
